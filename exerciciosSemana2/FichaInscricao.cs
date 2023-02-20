e11dab927349f81fa678afa710bb416b27c265cc
Lucas <lucas_baffi@estudante.sesisenai.org.br>
2023-02-20 19:15:35 -0300

    exercicio1

---
+++ exerciciosSemana2/FichaInscricao.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+namespace Ficha
+{
+    
+        
+public class FichaInscricao
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public bool MenorIdade { get; set; }
+
+    public void ImprimirDados()
+    {
+        Console.WriteLine("Código {0}, Nome {1}, Data Nascimento {2}", Id, Nome, DataNascimento);
+    }
+}
+    }
--- exerciciosSemana2/Program.cs
+++ exerciciosSemana2/Program.cs
@@ -1,7 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-class nomeDaClasse
-{
-    
-}
+// Criar uma classe chamada FichaInscricao
+// Detalhes da classe
+
+// Modificador de acesso publico
+// Propriedades
+// Id tipo inteiro (int)
+// Nome tipo texto (string)
+// DataNascimento tipo DataHora (datetime)
+// MenorIdade tipo boleano (bool)
+// Criar um método que escreva no console o texto com os campos da Classe
+// Exemplo. Código {Id}, Nome {Nome}, Data Nascimento {DataNascimento}
+// Instanciar a classe no console conforme mostrado em aula
+// criar um objeto da classe FichaInscricao
+
+using Ficha;
+
+FichaInscricao minhaFicha = new FichaInscricao();
+
+// atribuir valores às propriedades do objeto
+minhaFicha.Id = 1;
+minhaFicha.Nome = "João";
+minhaFicha.DataNascimento = new DateTime(2000, 1, 1);
+minhaFicha.MenorIdade = false;
+
+// chamar o método ImprimirDados() para imprimir os dados do objeto no console
+minhaFicha.ImprimirDados();
+
